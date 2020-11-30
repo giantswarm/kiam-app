e7706e65195ce97c28d91c28b67e75b00dfa9fe6
Jihyuk Bok <tomahawk28@users.noreply.github.com>
2020-11-30 09:44:13 +0100

    Use apptest library and refactor tests (#56)
    * update circleci
    
    * update go mod
    
    * update integration test
    
    * update go mod
    
    * fix error
    
    * fix error log
    
    * fix error log
    
    * logging deployment name
    
    * extend waiting time before daemonset deployed
    
    * update helm values
    
    * using new values into apptest
    
    * nodeSelector value as raw yaml string
    
    * rollback nodeSelector
    
    * check daemonset exists
    
    * fix the function name and logging

--- go.mod
+++ go.mod
@@ -3,12 +3,9 @@ module github.com/giantswarm/kiam-app
 go 1.14
 
 require (
-	github.com/giantswarm/appcatalog v0.3.0
-	github.com/giantswarm/e2esetup v0.3.0
-	github.com/giantswarm/e2etests v0.3.1
-	github.com/giantswarm/helmclient v1.0.3
-	github.com/giantswarm/k8sclient v0.2.0
+	github.com/giantswarm/apptest v0.7.1
+	github.com/giantswarm/backoff v0.2.0
 	github.com/giantswarm/microerror v0.2.1
 	github.com/giantswarm/micrologger v0.3.4
-	k8s.io/apimachinery v0.17.2
+	k8s.io/apimachinery v0.18.9
 )
--- integration/test/basic/basic_test.go
+++ integration/test/basic/basic_test.go
@@ -4,58 +4,53 @@ package basic
 
 import (
 	"context"
-	"fmt"
-	"os"
 	"testing"
+	"time"
 
-	"github.com/giantswarm/appcatalog"
-	"github.com/giantswarm/helmclient"
+	"github.com/giantswarm/backoff"
 	"github.com/giantswarm/microerror"
-	"github.com/giantswarm/micrologger"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
-func TestHelm(t *testing.T) {
+// TestDaemonSets ensures that kiam-server and kiam-agent daemonset are created
+func TestDaemonSets(t *testing.T) {
 	ctx := context.Background()
+	var err error
 
-	// Install cert-manager so kiam certs can be issued.
-	err := installCertManager(ctx, helmClient, l)
+	err = checkDaemonsetExists(ctx)
 	if err != nil {
-		t.Fatalf("%#v", err)
-	}
-
-	err = ba.Test(context.Background())
-	if err != nil {
-		t.Fatalf("%#v", err)
+		t.Fatalf("could not get kiam-agent & kiam-server: %v", err)
 	}
 }
 
-func installCertManager(ctx context.Context, helmClient helmclient.Interface, logger micrologger.Logger) error {
-	tarballURL, err := appcatalog.GetLatestChart(ctx, catalogURL, certManagerAppName, "")
-	if err != nil {
-		return microerror.Mask(err)
-	}
+func checkDaemonsetExists(ctx context.Context) error {
+	var err error
 
-	tarballPath, err := helmClient.PullChartTarball(ctx, tarballURL)
-	if err != nil {
-		return microerror.Mask(err)
-	}
+	l.LogCtx(ctx, "level", "debug", "message", "waiting for daemonset create")
 
-	defer func() {
-		err := os.Remove(tarballPath)
-		if err != nil {
-			logger.LogCtx(ctx, "level", "debug", "message", fmt.Sprintf("deletion of %#q failed", tarballPath), "stack", microerror.JSON(err))
+	o := func() error {
+		_, err := appTest.K8sClient().AppsV1().DaemonSets(metav1.NamespaceSystem).Get(ctx, kiamServer, metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			return microerror.Maskf(executionFailedError, "daemonset %#q in %#q not found", kiamServer, metav1.NamespaceSystem)
 		}
-	}()
 
-	options := helmclient.InstallOptions{
-		ReleaseName: certManagerAppName,
+		_, err = appTest.K8sClient().AppsV1().DaemonSets(metav1.NamespaceSystem).Get(ctx, kiamAgent, metav1.GetOptions{})
+		if apierrors.IsNotFound(err) {
+			return microerror.Maskf(executionFailedError, "daemonset %#q in %#q not found", kiamAgent, metav1.NamespaceSystem)
+		}
+
+		return nil
 	}
+	b := backoff.NewConstant(2*time.Minute, 5*time.Second)
+	n := backoff.NewNotifier(l, ctx)
 
-	err = helmClient.InstallReleaseFromTarball(ctx, tarballPath, metav1.NamespaceSystem, map[string]interface{}{}, options)
+	err = backoff.RetryNotify(o, b, n)
 	if err != nil {
 		return microerror.Mask(err)
 	}
+
+	l.LogCtx(ctx, "level", "debug", "message", "daemonsets are created")
 
 	return nil
 }
